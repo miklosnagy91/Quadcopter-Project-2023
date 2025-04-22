c5b3e15279be78090be9b466da1420fcac9de639
miklosnagy91 <74212260+miklosnagy91@users.noreply.github.com>
2025-04-22 17:00:02 -0600

    Update states.h

--- Quadcopter Code/states.h
+++ Quadcopter Code/states.h
@@ -1,30 +1,36 @@
 #ifndef States_h
 #define States_h
 
-struct States{
-  States(){
-  yaw = -1;
-  yawWanted = -1;
-  yawOld = -1;
-  pitch = -1;
-  roll = -1; 
-  calibration = -1;
-  altitude = -1;
-  pidYawInput = -1;
-  pidRollInput = -1;
-  pidPitchInput = -1;
-  }
-  
+#include <stdint.h>
+
+struct States {
+  // Constructor
+  States()
+    : yaw(-1), yawWanted(-1), yawOld(-1),
+      pitch(-1), roll(-1), calibration(0),
+      altitude(-1), pidYawInput(-1),
+      pidRollInput(-1), pidPitchInput(-1) {}
+
+  // Orientation
   float yaw;
   float yawWanted;
   float yawOld;
   float pitch;
-  float roll; 
+  float roll;
+
+  // Sensor status
   uint8_t calibration;
+
+  // Environmental
   float altitude;
+
+  // PID inputs
   float pidYawInput;
   float pidRollInput;
   float pidPitchInput;
-}states;
+};
+
+// Declare a global instance
+extern States states;
 
 #endif
